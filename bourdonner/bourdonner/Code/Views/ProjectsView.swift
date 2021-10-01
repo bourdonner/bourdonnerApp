51b87315e267033d8fc1f19c03e7637a82df7615
Erick Martins <bourdonner1@gmail.com>
2021-10-01 18:00:08 -0300

    Pequenos ajustes

--- bourdonner/bourdonner/Code/Views/ProjectsView.swift
+++ bourdonner/bourdonner/Code/Views/ProjectsView.swift
@@ -11,33 +11,22 @@ struct ProjectsView : View{
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View{
-        VStack{
-     
-//            Text(type.rawValue)
-//                    .font(Omnes.bold.font(size: 21))
-//                    .opacity(1)
             
-                ScrollView {
-                    ProjectsListView(projectListFilter: type)
-                    
-                }
-            Spacer()
-
-        }
-        .navigationBarTitle(type.rawValue, displayMode: .inline)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: Button(action: {
-            handleBackButton()}, label: {
+            VStack{
+                    ScrollView {
+                        ProjectsListView(projectListFilter: type)
+                    }
+                Spacer()
+            }
+       
+            .navigationBarTitle(type.rawValue, displayMode: .inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action: {
+                handleBackButton()}, label: {
                 Image(systemName: "chevron.left")}))
-        
-
-    }
+        }
 
     func handleBackButton(){
         presentationMode.wrappedValue.dismiss()
-        
     }
-    
 }
-
-
